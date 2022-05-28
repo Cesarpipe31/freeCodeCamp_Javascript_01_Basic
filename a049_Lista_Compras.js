e0f4a844d4b0d99f79e5f9a1160c2f9609deb9e6
Cesarpipe31 <98058249+Cesarpipe31@users.noreply.github.com>
2022-05-28 13:11:48 -0500

    freeCodeCamp Javascrip 01 Basic 050

---
+++ a049_Lista_Compras.js
@@ -0,0 +1,19 @@
+// Lista de compras
+// Crea una lista de compras en la variable myList. La lista debe ser un arreglo multidimensional que
+// contenga varios sub-arreglos.
+
+// El primer elemento de cada sub-arreglo debe contener una cadena con el nombre del artículo.
+// El segundo elemento debe ser un número que represente la cantidad, por ejemplo.
+
+// ["Chocolate Bar", 15]
+// Debe haber al menos 5 sub-arreglos en la lista.
+
+//const myList = [];
+
+const myList = [
+  ["Papa", 100],
+  ["Arroz", 10],
+  ["Huevos", 30],
+  ["sal", 3],
+  ["Yuca", 20],
+]
