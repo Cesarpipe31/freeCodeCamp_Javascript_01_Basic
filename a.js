302eada90844a100338c3fe0242db79624e536a7
Cesarpipe31 <98058249+Cesarpipe31@users.noreply.github.com>
2022-05-30 20:45:51 -0500

    freeCodeCamp Javascrip 01 Basic 103

---
+++ a.js
@@ -0,0 +1,31 @@
+const ourArray1 = []
+let i = 0
+
+do {
+  ourArray1.push(i)
+  i++
+} while (i < 5)
+
+///
+
+const ourArray2 = []
+let j = 5
+
+while (j < 5) {
+  ourArray2.push(j)
+  j++
+}
+
+///
+
+const ourArray3 = []
+let m = 5
+
+do {
+  ourArray3.push(m)
+  m++
+} while (m < 5)
+
+console.log(ourArray1)
+console.log(ourArray2)
+console.log(ourArray3)
